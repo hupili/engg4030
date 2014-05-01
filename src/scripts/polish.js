28c59eef06eddaa88ec565294b98d3926cccd3e4
HU Pili <hpl1989@gmail.com>
2014-05-01 16:44:54 +0800

    basic working with jquery.fixedTOC plugin

--- src/scripts/polish.js
+++ src/scripts/polish.js
@@ -1,15 +1,15 @@
 'user strict';
 
 $(function(){
-    if ($('#toc').css('position') == 'fixed'){
-        $('#toc .list').hide();
-        $('#toc').on('mouseover', function(){
-            $(this).find('.list').show();
-            $(this).css('width', '15em');
-        });
-        $('#toc').on('mouseleave', function(){
-            $(this).find('.list').hide();
-            $(this).css('width', '3em');
-        });
-    }
+    //if ($('#toc').css('position') == 'fixed'){
+        //$('#toc .list').hide();
+        //$('#toc').on('mouseover', function(){
+            //$(this).find('.list').show();
+            //$(this).css('width', '15em');
+        //});
+        //$('#toc').on('mouseleave', function(){
+            //$(this).find('.list').hide();
+            //$(this).css('width', '3em');
+        //});
+    //}
 });
